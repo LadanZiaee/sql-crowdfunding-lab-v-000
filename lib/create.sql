48d6cfa8eb1b2cd49c1c76342ff7829b475b12d5
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-18 17:19:01 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER,
+);
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledge(
+amount INTEGER, user_id, project_id);
